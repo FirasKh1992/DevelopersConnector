c873b194258ad330b0fc6b3e304016715d6083c4
FirasKh1992 <firask53@gmail.com>
2021-06-27 19:53:52 +0300

    User Registeration

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db=config.get('mondgoURI');
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect(db,{useNewUrlParser:true});
+        await mongoose.connect(db,{ useUnifiedTopology: true ,useNewUrlParser: true});
         console.log("mongoDB connected....");
     }catch(err){
         console.error(err.message);
