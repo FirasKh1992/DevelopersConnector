ad1de0bf6c472b56bb2490c03efac1c7d7788f8a
FirasKh1992 <firask53@gmail.com>
2021-06-27 17:13:28 +0300

    connecting the server to mongoDB and add the routes

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express=require('express');
-const app = express();//initialize our app variable with express
+const express = require('express');
+const app = express(); //initialize our app variable with express
+const connectDB = require('./config/db');
+
+connectDB();
+app.get('/', (req, res) => res.send('API running'));
 
 
-app.get('/',(req,res)=>res.send('API running'));
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/post',require('./routes/api/post'));
+app.use('/api/profile',require('./routes/api/profile'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
