5bb750ebdabee4aeb4b6865c34877613c57692f5
FirasKh1992 <firask53@gmail.com>
2021-06-28 13:11:15 +0300

    add login authintication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express=require('express');
 const router=express.Router();
 const auth=require('../../middleware/auth');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config=require('config');
+const bcrypt=require('bcryptjs');
 
 //@route GET api/auth
 //@desc Test route
@@ -17,4 +21,51 @@ try{
 }
 );
 
+//@route post api/auth
+//@desc authenticate a user & get token 
+//@access  public
+router.post(
+    '/',
+    [
+      check('email', 'please include a valid email').isEmail(),
+      check(
+        'password',
+        'Password is required'
+      ).exists(),
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+      const {  email, password } = req.body;
+      try {
+        //see if user exists
+        let user = await User.findOne({ email });
+  
+        if (!user) {
+          return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+        
+  
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn:3600},
+            (err,token)=>{
+                if(err) throw err;
+                res.json({token});
+            })
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    }
+  );
 module.exports=router;
