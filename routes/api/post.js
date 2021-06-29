238491b018c4b3b06a9de18d6d3cca2362eca968
FirasKh1992 <firask53@gmail.com>
2021-06-29 16:57:29 +0300

    add new post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,39 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
+//@route post api/post
+//@desc create a post
+//@access  private
+router.post(
+  '/',
+  [auth, [check('text', 'adding text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
 
-//@route GET api/post
-//@desc Test route
-//@access  public 
-router.get('/',(req,res)=>res.send('post Route'));
+    try {
+      const user = await User.findById(req.user.id).select('-password'); //find the user and get it from db without the password
 
-module.exports=router;
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
