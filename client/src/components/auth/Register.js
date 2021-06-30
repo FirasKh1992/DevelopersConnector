d47cc7ce8c749e222820cb491d3c38657639415a
FirasKh1992 <firask53@gmail.com>
2021-06-30 21:14:33 +0300

    register example and login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState } from 'react';
+import {Link} from 'react-router-dom';
+
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,19 +14,23 @@ const Register = () => {
   const onChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-const onSubmit=e=>{
+  const onSubmit = async e => {
     e.preventDefault();
-    if(password!==password2){
-        console.log('passwords does not match');
-    }else console.log(formData);
-}
+    if (password !== password2) {
+      console.log('passwords does not match');
+    } else {
+      
+       console.log('Success');
+    }
+  };
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
       <p className='lead'>
         <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form className='form' onSubmit={e=>onSubmit(e)}>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='text'
@@ -72,7 +78,7 @@ const onSubmit=e=>{
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
