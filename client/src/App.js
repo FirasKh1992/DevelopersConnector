f285949b0cd675e812f063f2b6e6ecb25834ca53
FirasKh1992 <firask53@gmail.com>
2021-07-05 18:30:56 +0300

    add protected Route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layouts/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import store from './store';
@@ -30,6 +32,7 @@ const App = () => {
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
           </Switch>
         </Fragment>
       </Router>
